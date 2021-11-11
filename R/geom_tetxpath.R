179c1791ec9f838e3790e7d53cc5c5eab7b5aff8
Allan Cameron <CAMERAL931@xggc.scot.nhs.uk>
2021-11-11 18:17:46 +0000

    CRAN check passed

--- R/geom_tetxpath.R
+++ R/geom_tetxpath.R
@@ -10,8 +10,51 @@
 ##---------------------------------------------------------------------------##
 
 
-#' Create text along a path
-#' @description Allows text to follow a curved path in ggplot
+## -----------------------------------------------------------------------------
+#' Add Curved Text Along Paths in \code{ggplot2}
+#'
+#' @description The existing text-based geom layers in ggplot2
+#' (\code{geom_text} and \code{geom_label}) are ideal for the majority of plots,
+#' since typically textual annotations are short, straight and in line with the
+#' axes of the plot. However, there are some occasions when it is useful to have
+#' text follow a curved path. This may be to create or recreate a specific
+#' visual effect, or it may be to label a circular / polar plot in a more
+#' "natural" way.
+#'
+#' There are limitations inherent in the plotting of text elements in
+#' \code{ggplot} due to the way that the underlying \code{grid} graphics handles
+#' text. A text string is dealt with as a zero-width object, and therefore the
+#' rotation and spacing of the letters making up the string can only be dealt
+#' with by treating each letter separately. Inevitably, this means
+#' that curved text paths have to be calculated based on the size and aspect
+#' ratio of the plotting device. Resizing the device after drawing a curved
+#' text path will therefore cause artefacts of spacing and rotation in the text.
+#'
+#' It is important to realise that the letters are only rotated, and do not
+#' undergo any change in shape. Thus, for example, large text appearing on
+#' convex curves will not be deformed so that individual letters are narrower at
+#' the bottom and wider at the top. Doing so would require reinterpreting the
+#' letters as polygons.
+#'
+#' Another issue is that we may wish to use a short curved label on a much
+#' longer path. Spacing the letters equally along the path would mean there is
+#' too much space between the letters for the label to remain legible. A single
+#' text string is therefore kept "together" according to the point size of the
+#' text in \code{geom_textpath}. This then leaves the problem of where on the
+#' path the text should be placed. This can be dealt with by the aesthetic
+#' mapping \code{start_proportion}, which allows the user to place the labels
+#' at the desired position along the path, including separate positions for
+#' each label.
+#'
+#' A final point to note is that a path is usually a group-based geom (i.e.
+#' a path typically comprises x, y points from two columns over several rows of
+#' a data frame), whereas text labels can come from single rows in a data frame.
+#' This means that if we have a data frame with an x column, a y column and a
+#' grouping variable column, there can only be a single label for the group.
+#' Typically, this will be the grouping variable itself (see the examples,
+#' particularly those using the built-in \code{iris} data set.)
+#'
+#'
 #' @inheritParams ggplot2::layer
 #' @param ... other arguments passed on to \code{\link{layer}}. These are often
 #' aesthetics, used to set an aesthetic to a fixed value, like \code{color =
@@ -19,10 +62,30 @@
 #'  geom/stat.
 #' @param start_proportion how far along the path should the text start?
 #' This is useful for situations with longer paths and shorter strings.
+#' @param na.rm Removes missing points or labels from the text path.
+#'
+#' @section Aesthetics:
+#' \code{geom_textpath()} understands the following aesthetics (required aesthetics are in bold):
+#' \itemize{
+#'   \item \strong{\code{x}}
+#'   \item \strong{\code{y}}
+#'   \item \strong{\code{label}}
+#'   \item \code{alpha}
+#'   \item \code{colour}
+#'   \item \code{family}
+#'   \item \code{fontface}
+#'   \item \code{group}
+#'   \item \code{hjust}
+#'   \item \code{size}
+#'   \item \code{vjust}
+#' }
+#'
 #' @export
 #'
-#'
 #' @examples
+#'
+#' # Plot text along a spiral path
+#'
 #' spiral <- data.frame(x = rev(sin(seq(0, 5*pi, length.out = 1000)) * 1000:1),
 #'                      y = rev(cos(seq(0, 5*pi, length.out = 1000)) * 1000:1),
 #'                      s = seq(1, 10, length.out = 1000),
@@ -37,6 +100,8 @@
 #'   theme_void()
 #'
 #'
+#' # Straight text paths in Cartesian Co-ordinates curve in Polar Co-ordinates
+#'
 #' df <- data.frame(x = 1:1000, y = 1, z = "This is a perfectly flat label")
 #'
 #' p <- ggplot(df, aes(x, y, label = z)) +
@@ -45,6 +110,29 @@
 #' p
 #'
 #' p + coord_polar()
+#'
+#'
+#' # Plot labels within groups of points using stat = "smooth"
+#'
+#' ggplot(iris, aes(x = Sepal.Length, y = Petal.Length, color = Species)) +
+#'   geom_point(alpha = 0.5) +
+#'   geom_textpath(aes(label = Species,
+#'                 color = stage(Species, after_scale = darken(color, 1.5))),
+#'                 size = 5, stat = "smooth",
+#'                 start_proportion = 0.25, fontface = 2)
+#'
+#'
+#' # Plot labels along density curves using stat = "density"
+#'
+#' ggplot(iris, aes(x = Sepal.Length, color = Species)) +
+#'   geom_density(alpha = 0.5) +
+#'   geom_textpath(aes(label = as.character(Species),
+#'                     color = stage(Species, after_scale = darken(color, 1.5)),
+#'                     vjust = -0.1),
+#'                 size = 5, stat = "density",
+#'                 start_proportion = 0.12, fontface = 2)
+#'
+
 geom_textpath <- function(mapping = NULL, data = NULL, stat = "identity",
                           position = "identity", na.rm = FALSE, show.legend = NA,
                           inherit.aes = TRUE,
@@ -54,6 +142,8 @@ geom_textpath <- function(mapping = NULL, data = NULL, stat = "identity",
         position = position, show.legend = show.legend, inherit.aes = inherit.aes,
         params = list(start_proportion = start_proportion, ...))
 }
+
+## -----------------------------------------------------------------------------
 
 .add_path_data <- function(.data)
 {
@@ -69,6 +159,7 @@ geom_textpath <- function(mapping = NULL, data = NULL, stat = "identity",
   .data
 }
 
+## -----------------------------------------------------------------------------
 .get_path_points <- function(path, x_start)
 {
   n       <- nrow(path)
@@ -94,6 +185,17 @@ geom_textpath <- function(mapping = NULL, data = NULL, stat = "identity",
   df$label <- letters
   df[!is.na(df$angle), ]
 }
+
+## -----------------------------------------------------------------------------
+
+#' The Geom object for a textpath
+#'
+#' This is the \code{ggproto} class that creates the textpath layer. It is not
+#' intended to be used directly by the end user.
+#'
+#' @format NULL
+#' @usage NULL
+#' @export
 
 GeomTextPath <- ggplot2::ggproto("GeomTextPath", ggplot2::Geom,
   required_aes = c("x", "y", "label"),
