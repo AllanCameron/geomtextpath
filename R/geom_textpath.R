50e4f3903dee3840a199adc02425aa0c9a8ffa99
Teun van den Brand <49372158+teunbrand@users.noreply.github.com>
2021-11-16 08:55:04 +0100

    Merge code

--- R/geom_textpath.R
+++ R/geom_textpath.R
@@ -205,9 +205,16 @@ geom_textpath <- function(
   # the spacing between characters when vjust is used, otherwise the spacing
   # will change
 
-  curvature <- diff(atan(.data$grad))/diff(.data$length)
-  .data$curvature <- approx(seq_along(curvature), grad,
-                 seq(1, length(curvature), length = length(curvature) + 1))$y
+  rad_diff <- diff(atan(.data$grad))
+  rad_diff <- rad_diff[abs(rad_diff) < pi/2]
+  rad_diff <- approx(seq_along(rad_diff), rad_diff,
+                     seq(1, length(rad_diff), length.out = nrow(.data) - 1))$y
+  curvature <- rad_diff/diff(.data$length)
+
+  effective_length <- diff(.data$length) * (1 + (.data$vjust[1] - 0.5) * 0.04 *curvature)
+
+  .data$adj_length <- c(0, cumsum(effective_length))
+
 
   # It will be useful to keep a record of the total length of the string we
   # wish to write. This will be used to normalise the width of the component
@@ -257,7 +264,7 @@ geom_textpath <- function(
 
   # This calculates the starting distance along the path where we place
   # the first letter
-  start_dist <- path$hjust[1] * (max(path$length) - max(letterwidths))
+  start_dist <- path$hjust[1] * (max(path$adj_length) - max(letterwidths))
 
   # Now we just add on the letterwidths and we have the correct distances
   dist_points <- letterwidths + start_dist
@@ -267,7 +274,7 @@ geom_textpath <- function(
   # be identical, so these are just kept as-is
   df <- as.data.frame(lapply(path, function(i) {
     if(is.numeric(i))
-      approx(x = path$length, y = i, xout = dist_points, ties = mean)$y
+      approx(x = path$adj_length, y = i, xout = dist_points, ties = mean)$y
     else
       rep(i[1], length(dist_points))
     }))
