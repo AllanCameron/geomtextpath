226cbb37b33271baf202b4b88e60511c6d4f2a2a
Teun van den Brand <49372158+teunbrand@users.noreply.github.com>
2021-12-09 21:15:17 +0100

    Cleanup text helpers

--- R/text_helpers.R
+++ R/text_helpers.R
@@ -15,95 +15,6 @@
 #'
 #' @examples
 #' measure_text("Hello there,\nGeneral Kenobi")
-measure_text_old <- function(label, gp = gpar(), ppi = 72,
-                         vjust = 0.5, hjust = 0, halign = "center") {
-
-  halign <- match(halign, c("center", "left", "right"), nomatch = 2L)
-  halign <- c("center", "left", "right")[halign]
-
-  if ({unit_vjust <- is.unit(vjust)}) {
-    offset_unit <- rep(vjust, length.out = length(label))
-    vjust  <- 0
-  }
-  # Remedy for https://github.com/r-lib/systemfonts/issues/85
-  vjust[vjust == 1] <- 1 + .Machine$double.eps
-
-  # Multiplier for sub-pixel precision
-  mult <- 100
-  # We need to call shape_string twice with lots of parameters which are mostly
-  # the same, so we store the parameters in a list and use do.call to avoid
-  # replication in the code.
-  string_args <- list(
-    strings    = label,
-    family     = gp$fontfamily %||% "",
-    italic     = (gp$font      %||% 1) %in% c(3, 4),
-    bold       = (gp$font      %||% 1) %in% c(2, 4),
-    size       = gp$fontsize   %||% 12,
-    lineheight = gp$lineheight %||% 1.2,
-    tracking   = gp$tracking   %||% 0,
-    res = ppi * mult,
-    vjust = vjust,
-    hjust = hjust,
-    align = halign
-  )
-
-  txt <- do.call(shape_string, string_args)
-
-  # Now we repeat the call to shape_string with a 0.5 vadjusted "x" to get the
-  # y offset. This allows us to correct for the fixed vjust of 0.5 passed to
-  # textGrob inside makeContent.textpath
-
-  string_args$strings <- rep("x", length(label))
-  string_args$vjust   <- 0.5
-  x_adjust <- do.call(shape_string, string_args)$shape$y_offset
-
-  # Adjust metrics
-  metrics <- txt$metrics
-  metrics$width  <- metrics$width  / (ppi * mult)
-  metrics$height <- metrics$height / (ppi * mult)
-
-  # Filter non-letters
-  txt <- txt$shape
-  txt <- txt[!(txt$glyph %in% c("\r", "\n", "\t")), , drop = FALSE]
-
-  # Combine glyphs of compound characters (#31)
-  txt <- resolve_composite_glyphs(txt)
-
-  # Adjust shape
-  txt$x_offset   <- txt$x_offset   / ppi
-  txt$x_midpoint <- txt$x_midpoint / ppi
-  txt$y_offset   <- (txt$y_offset - x_adjust[txt$metric_id + 1]) / ppi
-
-  # Format shape
-  ans <- data_frame(
-    glyph =  txt$glyph,
-    ymin  =  txt$y_offset,
-    xmin  =  txt$x_offset,
-    xmid  = (txt$x_offset + txt$x_midpoint),
-    xmax  = (txt$x_offset + txt$x_midpoint * 2)
-  )
-
-  # Split and assign group-specific attributes
-  ans <- split(ans, txt$metric_id)
-  ans <- lapply(seq_along(ans), function(i) {
-    df      <- ans[[i]]
-    offset  <- unique(c(0, df$ymin))
-    df$y_id <- match(df$ymin, offset)
-    if (unit_vjust) {
-
-      df$y_id <- df$y_id + 1
-      offset  <- unit(offset, "inch")
-
-      offset <- unit.c(unit(0, "inch"), offset + offset_unit[i])
-    }
-    attr(df, "metrics") <- metrics[i, , drop = FALSE]
-    attr(df, "offset")  <- offset
-    df
-  })
-
-  return(ans)
-}
-
 measure_text <- function(
   label,
   gp = gpar(),
@@ -196,35 +107,7 @@ measure_text <- function(
   return(ans)
 }
 
-index_cache <- new.env(parent = emptyenv())
-
-glyph_index <- function(family = "") {
-  name <- if (family == "") "_default_" else family
-  if (name %in% names(index_cache)) {
-    return(index_cache[[name]])
-  }
-  idx <- intToUtf8(1:64000, multiple = TRUE)
-  idx <- systemfonts::glyph_info(idx, family = family)
-  idx$int <- 1:64000
-  idx <- idx[idx$index > 0 & !is.na(idx$glyph), c("index", "int")]
-  ans <- rep(0L, max(idx$index) + 1)
-  ans[idx$index + 1] <- idx$int
-  index_cache[[name]] <- ans
-  ans
-}
-
-translate_glyph <- function(index, id, gp = gpar()) {
-  ints <- lapply(gp$fontfamily %||% "", glyph_index)
-  split(index, id) <- Map(
-    function(int, idx) int[idx + 1],
-    int = ints,
-    idx = split(index, id)
-  )
-  intToUtf8(index, multiple = TRUE)
-}
-
 # This is a simpler version of measure_text for expressions only
-
 measure_exp <- function(label, gp = gpar(), ppi = 72, vjust = 0.5)
 {
   size <- gp$fontsize %||% 11
@@ -257,35 +140,47 @@ measure_exp <- function(label, gp = gpar(), ppi = 72, vjust = 0.5)
   )
 }
 
-# Ideally remove the need for this function through dedicated text shaping
-# library. Currently, we have a hacky way of identifying modifications to a
-# base character through zero-width glyphs, which I don't expect to be robust
-# in all cases.
-resolve_composite_glyphs <- function(txt) {
-  if (!any({zero_width <- txt$x_midpoint == 0})) {
-    return(txt)
+# Here a cache that stores index lookup tables of fonts
+index_cache <- new.env(parent = emptyenv())
+
+# This function retrieves index lookup tables for fonts. If the font has been
+# seen before, it is retrieved from the cache. If not, it looks up the index
+# table and stores it in the cache.
+glyph_index <- function(family = "") {
+  # Empty character is invalid name
+  name <- if (family == "") "_default_" else family
+
+  # Retrieve from cache if it exists there
+  if (name %in% names(index_cache)) {
+    return(index_cache[[name]])
   }
-  # Need to find runs in case of multiple modifiers to the base character
-  run   <- rle(zero_width)
-  end   <- run_end(run$lengths, TRUE)
-  start <- end - run$lengths + 1
-  # Init vector for rows to remove afterwards
-  remo  <- integer(0)
-  # Runs of modifiers
-  mod_run <- setdiff(which(run$values), 1) # Exclude a first run
-  for (i in mod_run) {
-    # Current run of modifiers
-    this_run <- start[i]:end[i]
-    # Preceding base character
-    base_char <- this_run[1] - 1
-    # Combine glyphs
-    txt$glyph[base_char] <- paste0(
-      txt$glyph[base_char],
-      paste0(txt$glyph[this_run], collapse = "")
-    )
-    # Append rows to remove
-    remo <- c(remo, this_run)
-  }
-  # Remove rows of modifiers
-  txt[-remo, , drop = FALSE]
+
+  # Get all unicode characters
+  idx <- intToUtf8(1:64000, multiple = TRUE)
+  idx <- systemfonts::glyph_info(idx, family = family)
+  idx$int <- 1:64000
+
+  # Filter invalid glyphs
+  idx <- idx[idx$index > 0 & !is.na(idx$glyph), c("index", "int")]
+
+  # Format such that index + 1 gives correct answer
+  ans <- rep(0L, max(idx$index) + 1)
+  ans[idx$index + 1] <- idx$int
+
+  # Store in cache
+  index_cache[[name]] <- ans
+  ans
+}
+
+# This function does the translation of font indices to glyphs. Note that
+# different strings can have different fonts, which is why we need to loop
+# over the indices.
+translate_glyph <- function(index, id, gp = gpar()) {
+  ints <- lapply(gp$fontfamily %||% "", glyph_index)
+  split(index, id) <- Map(
+    function(int, idx) int[idx + 1],
+    int = ints,
+    idx = split(index, id)
+  )
+  intToUtf8(index, multiple = TRUE)
 }
