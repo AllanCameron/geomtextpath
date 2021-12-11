7c2cc8e6a729069af27c05bfca9110e51357b954
AllanCameron <Allan.Cameron@nhs.scot>
2021-12-11 10:41:35 +0000

    100% test coverage

--- tests/testthat/test-geom_labelcontour.R
+++ tests/testthat/test-geom_labelcontour.R
@@ -6,3 +6,12 @@ test_that("We can produce labelled contour plots", {
   expect_silent(ggplot(df, aes(x, y, z = z)) + geom_labelcontour())
   expect_silent(ggplot(df, aes(x, y, z = z)) + stat_labelcontour())
 })
+
+test_that("We can produce labelled 2d density plots", {
+
+  set.seed(1)
+
+  df <- data.frame(x = rnorm(100), y = rnorm(100))
+
+  expect_silent(ggplot(df, aes(x, y)) + geom_labeldensity2d())
+})
