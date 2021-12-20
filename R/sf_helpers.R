59218ede7303613a78cea81ac51bffa2cc91aa9e
AllanCameron <Allan.Cameron@nhs.scot>
2021-12-20 19:09:04 +0000

    Infrastructure for sf

---
+++ R/sf_helpers.R
@@ -0,0 +1,136 @@
+
+label_sf <- function(x, ...) UseMethod("label_sf")
+
+label_sf.default <- function(x, ...) x
+
+label_sf.sfc_LINESTRING <- function(x, label) {
+
+  label <- match_labels(x, label)
+  attr(x, "label") <- label
+  class(x)[class(x) == "sfc_LINESTRING"] <- "sfc_LINESTRING_labelled"
+  x
+}
+
+st_as_grob.sfc_LINESTRING_labelled <- function (
+  x, arrow = NULL, default.units = "npc", name = NULL,
+  gp = grid::gpar(), vp = NULL, ...)
+{
+
+  label <- attr(x, "label")
+  class(x)[class(x) == "sfc_LINESTRING_labelled"] <- "sfc_LINESTRING"
+
+  if(is.null(label)) return(sf::st_as_grob(x))
+  label <- as.character(label)[1]
+
+  if(!nzchar(label)) return(sf::st_as_grob(x))
+
+  is_e <- vapply(unclass(x), nrow, integer(1)) == 0
+  if (any(is_e)) {
+      gp = gp[!is_e]
+      x = x[!is_e]
+  }
+
+    if (length(x)) {
+        x <- unclass(x)
+        n_points <- vapply(x, nrow, integer(1))
+        id <- rep(seq_along(n_points), n_points)
+        x <- do.call(rbind, x)
+        textpathGrob(label = label[1], x = x[, 1], y = x[, 2],
+                     id = id, arrow = arrow,
+            default.units = default.units, name = name, gp_path = gp,
+            vp = vp)
+    }
+    else nullGrob()
+}
+
+sf_types <- c(GEOMETRY = "other", POINT = "point", LINESTRING = "line",
+              POLYGON = "other", MULTIPOINT = "point", MULTILINESTRING = "line",
+              MULTIPOLYGON = "other", GEOMETRYCOLLECTION = "collection",
+              CIRCULARSTRING = "line", COMPOUNDCURVE = "line",
+              CURVEPOLYGON = "other",
+              MULTICURVE = "line", MULTISURFACE = "other", CURVE = "line",
+              SURFACE = "other", POLYHEDRALSURFACE = "other", TIN = "other",
+              TRIANGLE = "other")
+
+
+sf_grob_labels <- function(x, labels, lineend = "butt", linejoin = "round",
+                           linemitre = 10, arrow = NULL, na.rm = TRUE) {
+  # Match labels to data
+  if (missing(labels)) labels <- ""
+  labels <- match_labels(x, labels)
+
+  # Get sf types
+  type          <- sf_types[sf::st_geometry_type(x$geometry)]
+  is_point      <- type == "point"
+  is_line       <- type == "line"
+  is_other      <- type == "other"
+  is_collection <- type == "collection"
+  type_ind      <- match(type, c("point", "line", "other", "collection"))
+
+  # Remove missing rows
+  remove <- rep_len(FALSE, nrow(x))
+  remove[is_point] <- find_missing(x, GeomPoint)[is_point]
+  remove[is_line]  <- find_missing(x, GeomPath)[is_line]
+  remove[is_other] <- find_missing(x, GeomPolygon)[is_other]
+  if (any(remove)) {
+    if (!na.rm) {
+      warn(paste(
+        "Removed ", sum(remove), " rows containing missing values (geom_sf)."
+      ))
+    }
+    x <- x[!remove, , drop = FALSE]
+    label <- label[!remove]
+    type_ind <- type_ind[!remove]
+    is_collection <- is_collection[!remove]
+  }
+
+  # Generate default aesthetics for different geometry types
+  defaults <- list(
+    GeomPoint$default_aes,
+    GeomLine$default_aes,
+    modify_list(GeomPolygon$default_aes,
+                list(fill = "grey90", colour = "grey35"))
+  )
+
+  defaults[[4]] <- modify_list(
+    defaults[[3]],
+    rename(GeomPoint$default_aes, c(size = "point_size", fill = "point_fill"))
+  )
+
+  default_names <- unique(unlist(lapply(defaults, names)))
+
+  defaults <- lapply(setNames(default_names, default_names), function(n) {
+    unlist(lapply(defaults, function(def) def[[n]] %||% NA))
+  })
+
+  # Set variables to build grobs
+  alpha <- x$alpha %||% defaults$alpha[type_ind]
+  col   <- x$colour %||% defaults$colour[type_ind]
+  col[is_point | is_line] <- alpha(col[is_point | is_line],
+                                   alpha[is_point | is_line])
+  fill <- x$fill %||% defaults$fill[type_ind]
+  fill <- alpha(fill, alpha)
+  size <- x$size %||% defaults$size[type_ind]
+  point_size <- ifelse(is_collection,
+                       x$size %||% defaults$point_size[type_ind],
+                       size)
+  stroke <- (x$stroke %||% defaults$stroke[1]) * .stroke / 2
+  fontsize <- point_size * .pt + stroke
+  lwd <- ifelse(is_point, stroke, size * .pt)
+  pch <- x$shape %||% defaults$shape[type_ind]
+  lty <- x$linetype %||% defaults$linetype[type_ind]
+  gp <- gpar(
+    col = col, fill = fill, fontsize = fontsize, lwd = lwd, lty = lty,
+    lineend = lineend, linejoin = linejoin, linemitre = linemitre
+  )
+
+  # Build grobs
+  out <- grid::gTree()
+
+  for(i in seq_along(x$geometry))
+  {
+    g <- label_sf(x$geometry[i], labels[i])
+    out <- addGrob(sf::st_as_grob(g, pch = pch, gp = gp, arrow = arrow))
+  }
+  out
+}
