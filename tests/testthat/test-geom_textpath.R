7582beafb29326d251a8dae1e5f0e4156e89ad2b
Teun van den Brand <49372158+teunbrand@users.noreply.github.com>
2021-12-26 22:17:29 +0100

    Move rich tests

--- tests/testthat/test-geom_textpath.R
+++ tests/testthat/test-geom_textpath.R
@@ -89,3 +89,45 @@ test_that("Text path grob has correct types", {
   expect_s3_class(grobs$children[[1]], "polyline")
   expect_s3_class(grobs$children[[2]], "text")
 })
+
+# Rich text ---------------------------------------------------------------
+
+test_that("Text path grob has correct types", {
+
+  df <- data_frame(
+    x = c(1, 2, 1, 2),
+    y = c(1, 2, 2, 1),
+    lab = rep(c("<span style='color:red'>red</span> text",
+                "<sub>sub</sub>text is<sup>super</sup>"), each = 2)
+  )
+
+  p <- ggplot(df, aes(x, y, label = lab)) +
+    geom_textpath(hjust = 0.25, rich = TRUE)
+
+  grobs <- layer_grob(p)[[1]]
+
+  expect_s3_class(grobs, "gTree")
+  expect_s3_class(grobs, "textpath")
+  expect_length(grobs$children, 0)
+
+  expect_equal(
+    grobs$textpath$label[[c(1, 1)]][-4],
+    unlist(strsplit("red text", ""))[-4]
+  )
+
+  expect_equal(
+    grobs$textpath$label[[c(2, 1)]][-8],
+    unlist(strsplit("subtext issuper", ""))[-8]
+  )
+
+  grobs <- makeContent(grobs)
+
+  expect_length(grobs$children, 2)
+  expect_s3_class(grobs$children[[1]], "polyline")
+  expect_s3_class(grobs$children[[2]], "text")
+
+  expect_equal(
+    grobs$children[[2]]$gp$col,
+    rep(c("red", "#000000"), times = c(3, 20))
+  )
+})
