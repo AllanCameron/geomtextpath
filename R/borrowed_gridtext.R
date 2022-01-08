faae15a8e636a42690eab7816ec83bf306199bce
AllanCameron <Allan.Cameron@nhs.scot>
2022-01-08 18:36:43 +0000

    simplified gridtext

--- R/borrowed_gridtext.R
+++ R/borrowed_gridtext.R
@@ -19,7 +19,6 @@
 # Tag processing ----------------------------------------------------------
 
 process_tags <- function(node, drawing_context) {
-
   tags <- names(node)
   new  <- lapply(seq_along(node), function(i) {
     dispatch_tag(node[[i]], tags[i], drawing_context)
@@ -29,83 +28,61 @@ process_tags <- function(node, drawing_context) {
 
 
 dispatch_tag <- function(node, tag, drawing_context) {
-
   if (is.null(tag) || tag == "") {
     process_text(node, drawing_context)
   } else {
-    switch(
-      tag,
-      "p"      = process_tag_p(node,   drawing_context),
-      "span"   = process_tag_p(node,   drawing_context),
-      "b"      = process_tag_b(node,   drawing_context),
-      "strong" = process_tag_b(node,   drawing_context),
-      "i"      = process_tag_i(node,   drawing_context),
-      "em"     = process_tag_i(node,   drawing_context),
-      "sub"    = process_tag_sub(node, drawing_context),
-      "sup"    = process_tag_sup(node, drawing_context),
-      abort(paste0(
+    call_name <- paste("process_tag", tag, sep = "_")
+    if(!exists(call_name)) {
+        abort(paste0(
         "The rich-text has a tag that isn't supported (yet): <", tag, ">\n",
         "Only a very limited number of tags are currently supported."
       ))
-    )
+    }
+    dc <- set_style(drawing_context, attr(node, "style"))
+    drawing_context <- call(call_name, node = node, drawing_context = dc)
   }
 }
 
 
 # Departs from gridtext:::process_text in that it returns a list-matrix
-process_text <- function(node, drawing_context) {
 
-  cbind(list(unlist(node)), list(drawing_context$gp),
+process_text <- function(node, drawing_context) {
+  cbind(list(unlist(node)),
+        list(drawing_context$gp),
         list(drawing_context$yoff))
 }
 
 
-process_tag_p <- function(node, drawing_context) {
-
-  attr <- attributes(node)
-  drawing_context <- set_style(drawing_context, attr$style)
+process_tag_p <- process_tag_span <-  function(node, drawing_context) {
   process_tags(node, drawing_context)
 }
 
 
-process_tag_b <- function(node, drawing_context) {
-
-  attr <- attributes(node)
-  drawing_context <- set_style(drawing_context, attr$style)
-  process_tags(node, set_context_font(drawing_context, 2))
+process_tag_b <- process_tag_strong <- function(node, drawing_context) {
+  drawing_context <- set_context_font(drawing_context, 2)
+  process_tags(node, drawing_context)
 }
 
 
-process_tag_i <- function(node, drawing_context) {
-
-  attr <- attributes(node)
-  drawing_context <- set_style(drawing_context, attr$style)
-  process_tags(node, set_context_font(drawing_context, 3))
+process_tag_i <- process_tag_em <- function(node, drawing_context) {
+  drawing_context <- set_context_font(drawing_context, 3)
+  process_tags(node, drawing_context)
 }
+
 
 # TODO: Are we handling nested <sub>/<sup> correctly in terms of y-offset?
 # Note that we departed here from {gridtext} in terms of how the y-offset is
 # handled
 process_tag_sub <- function(node, drawing_context) {
-
-  fontsize        <- 0.8 * drawing_context$gp$fontsize
-  drawing_context <- set_context_gp(drawing_context, gpar(fontsize = fontsize))
-  attr            <- attributes(node)
-  drawing_context <- set_style(drawing_context, attr$style)
-
-  drawing_context$yoff <- drawing_context$yoff - drawing_context$ascent / 2
+  drawing_context <- set_context_size(drawing_context, 0.8)
+  drawing_context <- set_context_offset(drawing_context, -0.5)
   process_tags(node, drawing_context)
 }
 
 
 process_tag_sup <- function(node, drawing_context) {
-
-  fontsize <- 0.8 * drawing_context$gp$fontsize
-  drawing_context <- set_context_gp(drawing_context, gpar(fontsize = fontsize))
-  attr <- attributes(node)
-  drawing_context <- set_style(drawing_context, attr$style)
-
-  drawing_context$yoff <- drawing_context$yoff + drawing_context$ascent / 2
+  drawing_context <- set_context_size(drawing_context, 0.8)
+  drawing_context <- set_context_offset(drawing_context,  0.5)
   process_tags(node, drawing_context)
 }
 
@@ -113,11 +90,14 @@ process_tag_sup <- function(node, drawing_context) {
 
 setup_context <- function(fontsize = 12, fontfamily = "", fontface = "plain",
                           colour = "black", lineheight = 1.2, gp = NULL) {
-
   if (is.null(gp)) {
     gp <- gpar(
-      fontsize = fontsize, fontfamily = fontfamily, fontface = fontface,
-      col = colour, cex = 1, lineheight = lineheight
+      fontsize   = fontsize,
+      fontfamily = fontfamily,
+      fontface   = fontface,
+      col        = colour,
+      cex        = 1,
+      lineheight = lineheight
     )
   }
   gp <- update_gpar(get.gpar(), gp)
@@ -126,7 +106,6 @@ setup_context <- function(fontsize = 12, fontfamily = "", fontface = "plain",
 
 
 update_gpar <- function(gp, gp_new) {
-
   names_new <- names(gp_new)
   names_old <- names(gp)
   gp[c(intersect(names_old, names_new), "fontface")] <- NULL
@@ -136,18 +115,12 @@ update_gpar <- function(gp, gp_new) {
 
 
 set_context_gp <- function(drawing_context, gp = NULL) {
-
   gp <- update_gpar(drawing_context$gp, gp)
-  update_context(
-    drawing_context,
-    ascent = x_height(gp),
-    gp = gp
-  )
+  update_context(drawing_context, ascent = x_height(gp), gp = gp)
 }
 
 
-set_context_font <- function(drawing_context, font = 1,
-                                 overwrite = FALSE) {
+set_context_font <- function(drawing_context, font = 1, overwrite = FALSE) {
   font_old <- drawing_context$gp$font
   old_bold <- font_old %in% c(2, 4)
   new_bold <- font     %in% c(2, 4)
@@ -166,9 +139,19 @@ set_context_font <- function(drawing_context, font = 1,
 }
 
 
-update_context <- function(drawing_context, ...) {
+set_context_size <- function(drawing_context, size = 1) {
+  fontsize        <- size * drawing_context$gp$fontsize
+  set_context_gp(drawing_context, gpar(fontsize = fontsize))
+}
 
-  dc_new <- list(...)
+
+set_context_offset <- function(drawing_context, offset = 0) {
+  drawing_context$yoff <- drawing_context$yoff + drawing_context$ascent * offset
+  drawing_context
+}
+
+update_context <- function(drawing_context, ...) {
+  dc_new    <- list(...)
   names_new <- names(dc_new)
   names_old <- names(drawing_context)
   drawing_context[intersect(names_old, names_new)] <- NULL
@@ -194,21 +177,20 @@ set_style <- function(drawing_context, style = NULL) {
   )
 }
 
+
 # CSS parsing -------------------------------------------------------------
 
 parse_css <- function(text) {
-
   lines <- strsplit(text, ";", fixed = TRUE)[[1]]
   unlist(lapply(lines, parse_css_line), FALSE)
 }
 
 
 parse_css_line <- function(line) {
-
   pattern <- "\\s*(\\S+)\\s*:\\s*(\"(.*)\"|'(.*)'|(\\S*))\\s*"
-  m <- attributes(regexpr(pattern, line, perl = TRUE))
-  start <- m$capture.start
-  end   <- start + m$capture.length - 1
+  m       <- attributes(regexpr(pattern, line, perl = TRUE))
+  start   <- m$capture.start
+  end     <- start + m$capture.length - 1
   if (start[1] > 0) {
     key <- substr(line, start[1], end[1])
   } else {
@@ -229,19 +211,16 @@ parse_css_line <- function(line) {
 
 
 parse_css_unit <- function(x) {
-
   pattern <- "^((-?\\d+\\.?\\d*)(%|[a-zA-Z]+)|(0))$"
-  m <- attributes(regexpr(pattern, x, perl = TRUE))
-  start <- m$capture.start
-  end   <- start + m$capture.length - 1
+  m       <- attributes(regexpr(pattern, x, perl = TRUE))
+  start   <- m$capture.start
+  end     <- start + m$capture.length - 1
   if (start[4] > 0) {
     # matched null value
     return(list(value = 0, unit = "pt"))
   } else {
     if (start[2] > 0) {
-      value <- as.numeric(
-        substr(x, start[2], end[2])
-      )
+      value <- as.numeric(substr(x, start[2], end[2]))
       if (start[3] > 0) {
         unit <- substr(x, start[3], end[3])
         return(list(value = value, unit = unit))
@@ -253,7 +232,6 @@ parse_css_unit <- function(x) {
 
 
 convert_css_unit_pt <- function(x) {
-
   u <- parse_css_unit(x)
   switch(
     u$unit,
