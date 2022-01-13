80c38be2184d6ef806490956808ce5f496b4a1a5
Teun van den Brand <49372158+teunbrand@users.noreply.github.com>
2022-01-13 10:51:16 +0100

    Adapt sf tests

--- tests/testthat/test-sf.R
+++ tests/testthat/test-sf.R
@@ -5,22 +5,23 @@ test_that("We can label an sfc_LINESTRING", {
   linestring <- waterways$geometry[2]
 
   # Ensure the geometry objects are of the correct class:
-  expect_equal(class(multipolygon), c("sfc_MULTIPOLYGON", "sfc"))
-  expect_equal(class(linestring), c("sfc_LINESTRING", "sfc"))
+  expect_s3_class(multipolygon, c("sfc_MULTIPOLYGON", "sfc"))
+  expect_s3_class(linestring, c("sfc_LINESTRING", "sfc"))
 
   multipolygon <- label_sf(multipolygon, "test")
   linestring1 <- label_sf(linestring, "test")
   linestring2 <- label_sf(linestring, "test", as_textbox = TRUE)
 
   # Multipolygon should be unaltered, linestring should change
-  expect_equal(class(multipolygon), c("sfc_MULTIPOLYGON", "sfc"))
-  expect_equal(class(linestring1), c("sfc_labelled", "sfc"))
+  expect_s3_class(multipolygon, c("sfc_MULTIPOLYGON", "sfc"))
+  expect_s3_class(linestring1, c("sfc_labelled", "sfc"))
 
-  expect_equal(class(linestring2), c("sfc_textbox", "sfc"))
+  expect_s3_class(linestring2, c("sfc_textbox", "sfc"))
   expect_equal(attr(linestring1, "label"), "test")
 })
 
 test_that("sf objects are converted to correct grob types", {
+  skip_if_not_installed("sf")
 
   multipolygon <- waterways$geometry[1]
   linestring <- waterways$geometry[2]
@@ -44,31 +45,31 @@ test_that("sf objects are converted to correct grob types", {
   doubler2 <- label_sf(doubler, "test", as_textbox = TRUE)
   nullstring2 <- linestring2
   nullstring2[[1]] <- sf::st_linestring()
-  nullstring2 <- label_sf(nullstring2, "test", as_texbox = TRUE)
+  nullstring2 <- label_sf(nullstring2, "test", as_textbox = TRUE)
 
-  linegrob <- st_as_grob(linestring1)
-  linegrob2 <- st_as_grob(linestring2)
-  multipolygongrob <- st_as_grob(multipolygon)
-  blank_linegrob <- st_as_grob(linestring_blank)
-  blank_linegrob2 <- st_as_grob(linestring2_blank)
-  doublegrob1 <- st_as_grob(doubler1)
-  doublegrob2 <- st_as_grob(doubler2)
-  nullgrob1 <- st_as_grob(nullstring1)
-  nullgrob2 <- st_as_grob(nullstring2)
-  null_label_grob <- st_as_grob(linestring_null_label)
-  null_label_grob2 <- st_as_grob(linestring2_null_label)
+  linegrob <- sf::st_as_grob(linestring1)
+  linegrob2 <- sf::st_as_grob(linestring2)
+  multipolygongrob <- sf::st_as_grob(multipolygon)
+  blank_linegrob <- sf::st_as_grob(linestring_blank)
+  blank_linegrob2 <- sf::st_as_grob(linestring2_blank)
+  doublegrob1 <- sf::st_as_grob(doubler1)
+  doublegrob2 <- sf::st_as_grob(doubler2)
+  nullgrob1 <- sf::st_as_grob(nullstring1)
+  nullgrob2 <- sf::st_as_grob(nullstring2)
+  null_label_grob <- sf::st_as_grob(linestring_null_label)
+  null_label_grob2 <- sf::st_as_grob(linestring2_null_label)
 
-  expect_equal(class(linegrob)[1], "textpath")
-  expect_equal(class(linegrob2)[1], "labelpath")
-  expect_equal(class(doublegrob1)[1], "textpath")
-  expect_equal(class(doublegrob2)[1], "labelpath")
-  expect_equal(class(nullgrob1)[1], "null")
-  expect_equal(class(nullgrob2)[1], "null")
-  expect_equal(class(blank_linegrob)[1], "polyline")
-  expect_equal(class(blank_linegrob2)[1], "polyline")
-  expect_equal(class(null_label_grob)[1], "polyline")
-  expect_equal(class(null_label_grob2)[1], "polyline")
-  expect_equal(class(multipolygongrob)[1], "pathgrob")
+  expect_s3_class(linegrob, "textpath")
+  expect_s3_class(linegrob2, "labelpath")
+  expect_s3_class(doublegrob1, "textpath")
+  expect_s3_class(doublegrob2, "labelpath")
+  expect_s3_class(nullgrob1, "null")
+  expect_s3_class(nullgrob2, "null")
+  expect_s3_class(blank_linegrob, "polyline")
+  expect_s3_class(blank_linegrob2, "polyline")
+  expect_s3_class(null_label_grob, "polyline")
+  expect_s3_class(null_label_grob2, "polyline")
+  expect_s3_class(multipolygongrob, "pathgrob")
 
 })
 
