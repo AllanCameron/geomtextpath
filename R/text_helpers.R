9aa4c2a852ba7dc21df46c4426a25c46791da948
Yihui Xie <xie@yihui.name>
2022-11-01 12:37:28 -0500

    use the new alias `mark_html()`

--- R/text_helpers.R
+++ R/text_helpers.R
@@ -317,9 +317,7 @@ parse_richtext <- function(
   old_gp <- gp
 
   if (md) {
-    text <- markdown::markdownToHTML(text = text,
-              options = "-standalone")
-
+    text <- markdown::mark_html(text = text, options = "-standalone")
   }
   # Deal with <br> now, they are a pain to handle after parsing
   text <- gsub("<br>", "\n", text, fixed = TRUE)
